459617fbebd3bbc40823a797a1556669a468be96
Nildem Buse Ertürk <nildembuse@gmail.com>
2024-01-03 09:39:37 +0300

    hero kismi olusuyor

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,13 @@
 import './App.css'
-import Navbar from './component/Navbar'
+import  Navbar  from './component/Navbar'
+import  Hero  from './component/Hero'
 function App() {
  
   return (
     <div className="App">
       <Navbar />
+      <Hero/>
+
     </div>
   )
 }
---
+++ src/component/Hero.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+
+import "/src/index.css"
+function Hero() {
+  return (
+    <section className="container">
+      <div className="content">
+        <h1 className="title">Merhaba</h1>
+        <p className="description">
+          Junior Frontend Developer. <br />
+          9 aydır bu mesleğin içindeyim <br /> kendimi geliştiriyorum.
+        </p>
+        <a href="mailto:myemail@email.com" className="contactBtn">
+          Contact Me
+        </a>
+      </div>
+      <img src="/src/assets/hero/hero.jpg" alt="Hero image of me" className="heroImg" />
+      <div className="topBlur" />
+      <div className="bottomBlur" />
+    </section>
+  );
+}
+export default Hero
