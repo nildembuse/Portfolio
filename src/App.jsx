e63109a5ada1b0dd06a2dcb24af0d71be5c96288
Nildem Buse Ertürk <nildembuse@gmail.com>
2024-01-04 16:09:13 +0300

    duzenlemeler yapiliyor

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,19 @@
 import './App.css'
 import  Navbar  from './component/Navbar'
 import  Hero  from './component/Hero'
+import About from './component/About'
+import Projects from './component/Projects'
+import Contact from './component/Contact'
+
 function App() {
  
   return (
     <div className="App">
       <Navbar />
-      <Hero/>
+      <Hero />
+      <About />
+      <Projects />
+      <Contact />
 
     </div>
   )
